eff20d5f12cc79fa2b29c2abbe4e85a6e42afda3
Jason Cheung <jasoncheung94@hotmail.com>
2022-09-21 22:07:10 +0100

    State: Add a go implementation

---
+++ behavioral/state/README.md
@@ -0,0 +1,16 @@
+## State
+
+The state pattern is a behavioral software design pattern that allows an object to alter its behavior when its internal state changes. This pattern is close to the concept of finite-state machines. The state pattern can be interpreted as a strategy pattern, which is able to switch a strategy through invocations of methods defined in the pattern's interface.
+
+The state pattern is used in computer programming to encapsulate varying behavior for the same object, based on its internal state. This can be a cleaner way for an object to change its behavior at runtime without resorting to conditional statements and thus improve maintainability.
+
+### Additional Resources
+
+- [State Wikipedia](https://en.wikipedia.org/wiki/State_pattern)
+
+### Examples
+
+| Language   | Description    | Status | Code Examples                              |
+| ---------- | -------------- | ------ | ------------------------------------------ |
+| Golang(Go) | Simple Example | ✅     | [main.go](./simple-example/golang/main.go) |
+| Python     |                |        |                                            |
---
+++ behavioral/state/simple-example/golang/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+)
+
+type state interface {
+	open(c *Connection)
+	close(c *Connection)
+}
+
+// CloseState is ConcreteState
+type CloseState struct{}
+
+func (cs CloseState) open(c *Connection) {
+	fmt.Println("open the connection")
+	c.setState(OpenState{})
+}
+
+func (cs CloseState) close(c *Connection) {
+	fmt.Println("connection is already closed")
+}
+
+// OpenState is ConcreteState
+type OpenState struct{}
+
+func (os OpenState) open(c *Connection) {
+	fmt.Println("connection is already open")
+}
+
+func (os OpenState) close(c *Connection) {
+	fmt.Println("close the connection")
+	c.setState(CloseState{})
+}
+
+// Connection is Context
+type Connection struct {
+	state state
+}
+
+// Open connection
+func (c *Connection) Open() {
+	c.state.open(c)
+}
+
+// Close connection
+func (c *Connection) Close() {
+	c.state.close(c)
+}
+
+func (c *Connection) setState(state state) {
+	c.state = state
+}
+
+func main() {
+
+	// Client
+	con := Connection{CloseState{}}
+	con.Open()
+	// printed: open the connection
+	con.Open()
+	// printed: connection is already open
+	con.Close()
+	// printed: close the connection
+	con.Close()
+	// printed: connection is already closed
+}
